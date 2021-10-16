a527ca514b258d734c685d2e5d0d9ba15136a73d
EC2 Default User <ec2-user@ip-172-31-27-131.ap-northeast-1.compute.internal>
2021-10-16 16:36:51 +0000

    ルーティング変更

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,23 @@
 class Admin < ApplicationRecord
+    resources :order_details, only:[:update]
+    resources :orders, only:[:index,:show,:update]
+    resources :genres, only:[:index,:create,:edit,:update]
+    resources :products, only:[:index,:new,:create,:show,:edit,:update]
+    resources :customers, only:[:index,:show,:edit,:update]
+    root :to => "homes#top"
+    
+    resources :products, only:[:index,:show,]
+   resources :shippings, only:[:index,:create,:edit,:update,:destroy]
+   resource :customers, only:[:show,:update]
+   get 'customer_edit' => 'customers#edit', as: 'customer_edit'
+   patch 'customers/update' => 'customers#update'
+   get 'customers/unsubscribe'
+   patch 'customers/withdraw'
+   get 'orders/thankyou'
+   resources :orders, only:[:new,:create,:index,:show]
+   post 'orders/comfirm'
+   delete 'cart_products/destroy_all'
+   resources :cart_products, only:[:create,:index,:update,:destroy]
+   root :to => "homes#top"
+   get 'homes/about'
 end
