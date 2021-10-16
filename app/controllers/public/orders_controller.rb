8038c50833f324e4aade0d2812e762070d93b7ad
EC2 Default User <ec2-user@ip-172-31-27-131.ap-northeast-1.compute.internal>
2021-10-16 06:13:41 +0000

    ルーティング設定　コントローラー設定

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,16 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def comfirm
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/public/shippings_controller.rb
@@ -0,0 +1,13 @@
+class Public::ShippingsController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
